5e30d804dcd73e8598355cdd4d9e8af062a8cc79
Brahim Lidrissi <blidriss@c3r8p12.1337.ma>
2026-06-24 16:05:15 +0100

    add containners :)

--- src/Graph.py
+++ src/Graph.py
@@ -1,17 +1,41 @@
 class Graph:
     def __init__(self):
         zones: dict = {}
+        start: Zone = None
+        end: Zone = None
 
 class Zone:
-    def __int__(
-            name: str, type: str, x: int, y: int,
-            color: str | None = None, max_dron: int = 1,
-            connections: list = []
+    def __init__(
+            self,
+            name: str,
+            zone_type: str,
+            x: int, y: int,
+            connections: list | None = None,
+            color: str | None = None,
+            max_dron: int = 1
             ):
-        pass
+        self.name = name
+        self.zone_type = zone_type
+        self.x = x
+        self.y = y
+        self.connections = connections or []
+        self.color = color
+        self.max_dron = max_dron
+    def __repr__(self):
+        conect = [zone.name for zone in self.conections]
+        return f"{self.name}: {conect or 'No connections'}"
+
 
 class Connetion:
     def __init__(
-            self, first: Zone, second: Zone, dron_capacity: int = 1
+            self,
+            first: Zone,
+            second: Zone,
+            dron_capacity: int = 1
             ):
-        pass
+        self.first = first
+        self.second = second
+        self.dron_capacity = dron_capacity
+    def __repr__(self):
+        size = self.dron_capacity
+        return f"{self.first} ===> {self.second} : dron_capacity={size}"
