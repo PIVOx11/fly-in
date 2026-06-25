5d84254480ab4e36ca3dd1632cdc1c1377c793ea
Brahim Lidrissi <blidriss@c3r8p14.1337.ma>
2026-06-25 20:48:43 +0100

    main

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ if __name__ == "__main__":
         parse = Parser(sys.argv[1])
     except ParsingError as e:
         print(f"[red]{e}[/red]")
+    try:
+        for i in parse.data_validate():
+            print(f"[green] {i}[/green]")
+    except ParsingError as e:
+        print(f"[red]{e}[/red]")
+
     
     # Now need to parse the data inside file :)
